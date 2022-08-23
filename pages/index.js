ba8c7f011a7f50701bb0ae307118e8e6eeb5d36c
James Bryan Maratas <111833106+JamesMaratas@users.noreply.github.com>
2022-08-23 16:35:02 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,9 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p> I'm James Bryan F. Maratas. My hobbies are playing basketball, online games, swimming, listening to music, dance, singing. My favorite food is buttered garlic shrimp.
- I take the IT course because it is my 3rd choice actually, my 1st and 2nd choice didn't go as it planned due to some financial problem so that's why my 3rd choice was the only choice that I had.
-I want to improve my learning and skills here in IT so I'll be able to produce enough and decent skills to work for a decent company. Hopefully I'll be able to strengthen my weaknesses and use them as my strenght 10 years from now.</p>
+        <p> I&apos;m James Bryan F. Maratas. My hobbies are playing basketball, online games, swimming, listening to music, dance, singing. My favorite food is buttered garlic shrimp.
+ I take the IT course because it is my 3rd choice actually, my 1st and 2nd choice didn&apos;t go as it planned due to some financial problem so that&apos;s why my 3rd choice was the only choice that I had.
+I want to improve my learning and skills here in IT so I%apos;ll be able to produce enough and decent skills to work for a decent company. Hopefully I&apos;ll be able to strengthen my weaknesses and use them as my strenght 10 years from now.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
