a6b6955959a4b40bde509db5c2a83bf8cd023e16
JamesMaratas <jbfmaratas.student@ua.edu.ph>
2022-08-23 16:25:44 +0800

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p> I'm James Bryan F. Maratas. My hobbies are playing basketball, online games, swimming, listening to music, dance, singing. My favorite food is buttered garlic shrimp.
+ I take the IT course because it is my 3rd choice actually, my 1st and 2nd choice didn't go as it planned due to some financial problem so that's why my 3rd choice was the only choice that I had.
+I want to improve my learning and skills here in IT so I'll be able to produce enough and decent skills to work for a decent company. Hopefully I'll be able to strengthen my weaknesses and use them as my strenght 10 years from now.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
